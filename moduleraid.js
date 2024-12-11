9ae606bfd21cc465e8dad35ef8b14360aa708a84
Nagashayan <nagashayan1@gmail.com>
2024-12-11 18:02:59 -0500

    Add english total and sanitize name

--- moduleraid.js
+++ moduleraid.js
@@ -131,41 +131,43 @@ window.addEventListener('message', (e) => {
     const poll = Store.Msg.getModelsArray().filter(m => m.type == "poll_creation").find(m => e.data.export.includes(m.__x_id.id))
     console.log(votes);
     console.log(poll);
-    let csv = "Name, Phone," + poll.__x_pollOptions.map(x => `"${x.name}"`).join(",") + "\n"
+
+    // Add the poll name as a row header
+    let csv = `"Poll Name: ${poll.__x_pollName}"\n`;
+
+    // Add column headers
+    csv += "Name, Phone," + poll.__x_pollOptions.map(x => `"${x.name}"`).join(",") + "\n";
 
     const voteAccumulator = votes.reduce((acc, x) => {
       x.__x_selectedOptionLocalIds.forEach(y => acc[y] = (acc[y] || 0) + 1)
       return acc
     }, {})
 
-    csv += ",סה\"כ,"
+    // Add total votes row with "Total" in English
+    csv += ",Total,";
 
     for (let i = 0; i < poll.__x__pollOptionsToLinks.size; i++) {
-      csv += voteAccumulator[i] ? voteAccumulator[i] : 0
-      csv += ","
+      csv += voteAccumulator[i] ? voteAccumulator[i] : 0;
+      csv += ",";
     }
-    csv += "\n"
+    csv += "\n";
 
-    csv += votes.map(x => ({
-      pollname: x.__x_pollName,
-      phone: x.__x_sender.user,
-      votes: x.__x_selectedOptionLocalIds,
-      name: Store.Contact.getModelsArray().find(y => y.__x_id.user == x.__x_sender.user).__x_pushname || Store.Contact.getModelsArray().find(y => y.__x_id.user == x.__x_sender.user).__x_name
-    }))
-      .map(x => {
+    // Add vote data rows
+    csv += votes.map(x => {
+      const rawName = Store.Contact.getModelsArray().find(y => y.__x_id.user == x.__x_sender.user).__x_pushname ||
+        Store.Contact.getModelsArray().find(y => y.__x_id.user == x.__x_sender.user).__x_name || "";
+      const sanitizedName = rawName.replace(/[^\w\s]/gi, '').trim();
 
-        let res = `"${x.name}"` + "," + x.phone
-        for (let i = 0; i < poll.__x__pollOptionsToLinks.size; i++) {
-          res += x.votes && x.votes.includes(i) ? ",X" : ","
-        }
-        return res
-      })
-      .join("\n")
+      let res = `"${sanitizedName}"` + "," + x.__x_sender.user;
+      for (let i = 0; i < poll.__x__pollOptionsToLinks.size; i++) {
+        res += x.votes && x.votes.includes(i) ? ",X" : ",";
+      }
+      return res;
+    }).join("\n");
 
-
-
+    // Download the CSV
     const a = document.createElement("a");
-    a.href = 'data:text/csv; charset=utf-8,' + encodeURIComponent("\uFEFF" + csv)
+    a.href = 'data:text/csv; charset=utf-8,' + encodeURIComponent("\uFEFF" + csv);
     a.download = "votes.csv";
     a.click();
   }
